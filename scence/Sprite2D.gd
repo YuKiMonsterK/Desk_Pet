5eef237c317b57969ba9b622639b7ddaa26b1e25
YuKiMonsterK <143022752+YuKiMonsterK@users.noreply.github.com>
2025-02-27 00:00:49 +0800

    能更改視窗位置

--- scence/Sprite2D.gd
+++ scence/Sprite2D.gd
@@ -1,11 +1,33 @@
 extends Sprite2D
-
-
-# Called when the node enters the scene tree for the first time.
+@onready var accept = $"../accept"
+@onready var books = $"../books"
+#當左鍵按到功能鍵時，press為true
+var move_area = false
+var move = Vector2i()
+# 當開始時執行
 func _ready():
-	get_tree().root.set_transparent_background(true)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
+
+
+# 每幀執行
+func _process(_delta):
+	accept.position = Vector2i(get_global_mouse_position())
+	
+# 有輸入時執行
+func _input(_event):
+	# 如果按下左鍵，且在上方移動區
+	if Input.is_action_pressed("accept") and move_area:
+		#更改遊戲窗口至左鍵位置
+		get_window().position = DisplayServer.mouse_get_position() + move
+
+
+func _on_accept_area_entered(_area):
+	pass
+
+# 如果進入上方移動區
+func _on_move_area_entered(area):
+	move_area = true
+	move = get_window().position - DisplayServer.mouse_get_position()
+# 如果離開上方移動區
+func _on_move_area_exited(area):
+	move_area = false
