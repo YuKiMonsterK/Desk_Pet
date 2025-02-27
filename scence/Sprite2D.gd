d304578be02988cfa7b8964539139b38293841d4
YuKiMonsterK <143022752+YuKiMonsterK@users.noreply.github.com>
2025-02-27 16:04:43 +0800

    修正視窗更改方法

--- scence/Sprite2D.gd
+++ scence/Sprite2D.gd
@@ -14,20 +14,22 @@ func _process(_delta):
 	accept.position = Vector2i(get_global_mouse_position())
 	
 # 有輸入時執行
-func _input(_event):
-	# 如果按下左鍵，且在上方移動區
-	if Input.is_action_pressed("accept") and move_area:
+func _input(event):
+	# 如果按下左鍵並移動，且在上方移動區
+	if Input.is_action_pressed("accept") and move_area and event is InputEventMouseMotion:
 		#更改遊戲窗口至左鍵位置
 		get_window().position = DisplayServer.mouse_get_position() + move
-
-
+	# 如果只按下左鍵，且在上方移動區
+	elif Input.is_action_pressed("accept") and move_area:
+		move = get_window().position - DisplayServer.mouse_get_position()
+		
 func _on_accept_area_entered(_area):
 	pass
 
 # 如果進入上方移動區
 func _on_move_area_entered(_area):
 	move_area = true
-	move = get_window().position - DisplayServer.mouse_get_position()
+	
 # 如果離開上方移動區
 func _on_move_area_exited(_area):
 	move_area = false
