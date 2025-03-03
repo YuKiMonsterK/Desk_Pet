473568d70abadb34386176f815538c4981bf0510
YuKiMonsterK <143022752+YuKiMonsterK@users.noreply.github.com>
2025-03-04 01:08:10 +0800

    重置專案，設定拖移框

--- script/function.gd
+++ script/function.gd
@@ -1,10 +1,6 @@
 extends Area2D
 
-@onready var box = $box
 
 func _ready():
-	box.visible = false
-	
-func _on_node_2d_interact():
-	print("receive")
-	box.visible = true
+	pass
+
--- tomato.gd
+++ tomato.gd
@@ -1,8 +1,6 @@
-extends Area2D
+extends Function
 
-@onready var box = $box
 
 func _on_node_2d_interact():
 	print("receive")
-	box.visible = true
 	
