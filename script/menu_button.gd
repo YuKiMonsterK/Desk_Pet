7768842e95fcf2a0624d3e67c62b718369591bf2
YuKiMonsterK <143022752+YuKiMonsterK@users.noreply.github.com>
2025-03-08 15:35:29 +0800

    能顯示菜單

--- script/menu_button.gd
+++ script/menu_button.gd
@@ -1,9 +1,18 @@
 extends "res://script/base_button.gd"
 
+@onready var canvas_layer = $CanvasLayer
+
+var box = false
 
 func _ready():
+	#統一監聽訊號管理腳本
 	SignalManager.connect("button_press", _on_button_press)
-
+	canvas_layer.visible = false
 func _on_button_press(node_name):
-	if node_name == self.name:
-		print("me")
+	#測試按鈕與對應訊息
+	if node_name == "MenuB" and not box:
+		print("I am MenuB")
+		canvas_layer.visible = true
+
+func _on_exit_button_down():
+	canvas_layer.visible = false
