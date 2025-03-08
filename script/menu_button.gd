784eaef65a7595e3b68db435579ea353437308d6
YuKiMonsterK <143022752+YuKiMonsterK@users.noreply.github.com>
2025-03-08 20:46:18 +0800

    菜單有退出功能

--- script/menu_button.gd
+++ script/menu_button.gd
@@ -1,6 +1,7 @@
 extends "res://script/base_button.gd"
 
-@onready var canvas_layer = $CanvasLayer
+@onready var panel_container = $PanelContainer
+
 #為之後的房間模式做準備
 @export var room_mode = false
 var box = false
@@ -8,12 +9,12 @@ var box = false
 func _ready():
 	#統一監聽訊號管理腳本
 	SignalManager.connect("button_press", _on_button_press)
-	canvas_layer.visible = false
+	panel_container.visible = false
 func _on_button_press(node_name):
 	#測試按鈕與對應訊息
 	if node_name == "MenuB" and not box:
 		print("I am MenuB")
-		canvas_layer.visible = true
+		panel_container.visible = true
 
 func _on_exit_button_down():
-	canvas_layer.visible = false
+	panel_container.visible = false
