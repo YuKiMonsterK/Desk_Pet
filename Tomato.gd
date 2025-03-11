aa335b4f243e57f426f5d809d81065e61e4312e2
YuKiMonsterK <143022752+YuKiMonsterK@users.noreply.github.com>
2025-03-12 00:52:02 +0800

    能依模式更改圖片

--- Tomato.gd
+++ Tomato.gd
@@ -1,6 +1,22 @@
 extends "res://script/menu_button.gd"
+@onready var tomato = $"."
 
+func _ready():
+	print(room_mode)
+	tomato.self_modulate = Color(1,1,1,1)
+	panel_container.visible = false
+	SignalManager.connect("button_press", _on_button_press)
+	if not room_mode:
+		tomato.icon = load("res://assets/測試的圖片資源/Tomato.jpg")
 func _on_button_press(node_name):
-	print(self.name)
+	print("self = ",self.name," node = ",node_name)
 	if node_name == self.name:
 		panel_container.visible = true
+
+func _on_mouse_entered():
+	if not room_mode:
+		tomato.self_modulate = Color(0.44,0.44,0.44,1)
+
+
+func _on_mouse_exited():
+	tomato.self_modulate = Color(1,1,1,1)
