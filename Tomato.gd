38f0b629f8d41c30438aa069ec60c37166973208
YuKiMonsterK <143022752+YuKiMonsterK@users.noreply.github.com>
2025-03-25 12:21:32 +0800

    fix bugs

--- Tomato.gd
+++ Tomato.gd
@@ -1,4 +1,5 @@
 extends "res://script/menu_button.gd"
+
 @onready var tomato = $"."
 @onready var icon_container = $PanelContainer
 @onready var collision_shape_2d = $move/CollisionShape2D
@@ -16,6 +17,7 @@ var move_p = Vector2()
 var move_edge = false
 var loop_cur = 0
 var current = "studing"
+
 func _ready():
 	print(room_mode)
 	panel_container.self_modulate = Color(1,1,1,1)
@@ -24,6 +26,7 @@ func _ready():
 	if not room_mode:
 		tomato.icon = load("res://assets/測試的圖片資源/Tomato.jpg")
 	collision_shape_2d.visible = false
+	
 func _process(_delta):
 	if icon_container.visible:
 		collision_shape_2d.visible = true
@@ -33,10 +36,12 @@ func _process(_delta):
 		collision_shape_2d.visible = false
 	if not Input.is_action_pressed("accept") and move_edge:
 		move_edge = false
+
 func _on_button_press(node_name):
 	print("self = ",self.name," node = ",node_name)
 	if node_name == self.name:
 		panel_container.visible = true
+		
 func _input(event):
 	# 如果按下左鍵並移動，且在移動區邊緣
 	if Input.is_action_pressed("accept") and move_edge and event is InputEventMouseMotion:
