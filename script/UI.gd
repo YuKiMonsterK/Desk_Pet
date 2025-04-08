f8766127dc323a96f4552d12949d3b6733c31c55
YuKiMonsterK <143022752+YuKiMonsterK@users.noreply.github.com>
2025-04-09 03:44:31 +0800

    新增角色相關配置

--- script/UI.gd
+++ script/UI.gd
@@ -3,6 +3,8 @@ extends Node2D
 @onready var game_window: Node2D = $game_window
 @onready var background_click_area: CollisionPolygon2D = $game_window/Sprite2D/click_area/CollisionPolygon2D
 @onready var background: Sprite2D = $game_window/Sprite2D
+@onready var character_body_2d: CharacterBody2D = $game_window/CharacterBody2D
+@onready var collision_shape_2d: CollisionShape2D = $game_window/CharacterBody2D/move/CollisionShape2D
 
 var move_edge = false
 var move_p = Vector2i()
@@ -12,6 +14,8 @@ var area_in = "null"
 var room_mode = true  # 為之後的房間模式做準備
 
 func _ready():
+	
+	collision_shape_2d.position = Vector2(-36.04,-197.078)
 	SignalManager.connect("room_mode", _on_room_mode)
 	SignalManager.connect("button_press", _on_button_press)
 	SignalManager.connect("exit_press", _on_exit_press)
@@ -22,6 +26,10 @@ func _ready():
 	game_window.position = screen_size - window_size
 	
 func _process(_delta):
+	
+	if not Input.is_action_pressed("accept") and collision_shape_2d.position == Vector2(-31.88,-217.878):
+		SignalManager.emit_signal("character_stop")
+		collision_shape_2d.position = Vector2(-36.04,-197.078)
 	if not Input.is_action_pressed("accept") and move_edge:
 		move_edge = false
 	var BG_click_polygon: PackedVector2Array = background_click_area.polygon.duplicate()
@@ -32,9 +40,11 @@ func _process(_delta):
 
 func _input(event):
 	if Input.is_action_pressed("accept") and move_edge and event is InputEventMouseMotion:
-		game_window.position = Vector2(DisplayServer.mouse_get_position()) + move_p
+		character_body_2d.position = Vector2(DisplayServer.mouse_get_position()) + move_p
+		collision_shape_2d.position = Vector2(-31.88,-217.878)
+		SignalManager.emit_signal("character_move")
 	elif Input.is_action_pressed("accept") and not move_edge:
-		move_p = game_window.position - Vector2(DisplayServer.mouse_get_position())
+		move_p = character_body_2d.position - Vector2(DisplayServer.mouse_get_position())
 
 func _on_move_mouse_exited():
 	move_edge = true
@@ -52,3 +62,5 @@ func _on_button_press(node_name):
 func _on_exit_press(node_name):
 	var node = game_window.get_node(node_name.replace("_button", ""))
 	node.visible = false
+
+	
