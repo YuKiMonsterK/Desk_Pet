5770f623d8ae29ad95d79b6066e7bf9c576e9e0f
YuKiMonsterK <143022752+YuKiMonsterK@users.noreply.github.com>
2025-05-05 22:39:24 +0800

    優化無背景模式排版，fix bugs

--- script/no_back_ui.gd
+++ script/no_back_ui.gd
@@ -3,7 +3,6 @@ extends Node2D
 @onready var game_window: Node2D = $game_window
 @onready var character_body_2d: CharacterBody2D = $game_window/CharacterBody2D
 @onready var collision_shape_2d: CollisionShape2D = $game_window/CharacterBody2D/move/CollisionShape2D
-@onready var timer: Timer = $Timer
 
 var move_edge = false #當滑鼠在拖移區的邊緣
 var move_p = Vector2i()
@@ -19,22 +18,15 @@ var p = Vector2(0,0)
 func _ready():
 	#初始化
 	collision_shape_2d.position = Vector2(-36.04,-197.078)
-	SignalManager.connect("room_mode", _on_room_mode) #連接信號
 	SignalManager.connect("button_press", _on_button_press)
 	SignalManager.connect("exit_press", _on_exit_press)
 	SignalManager.connect("colliding", _on_colliding)
-	SignalManager.connect("start_studing", _on_study)
-	SignalManager.connect("end_study", _end_study)
-	SignalManager.connect("start_studing", _on_studing)
+	SignalManager.connect("to_study", _to_study)
+	
 	var screen_size = get_viewport_rect().size
-	timer.wait_time = randf_range(5,15)
 	game_window.position = p
 func _process(_delta):
 	
-	#當沒移動且沒開計時且沒有目標位置
-	if not moving and timer.is_stopped() and character_p == 0 and not stop: 
-		timer.start()
-		
 	#在結束拖移時才會取消moving並回到停滯狀態
 	if not Input.is_action_pressed("accept") and collision_shape_2d.position == Vector2(-31.88,-217.878):
 		moving = false
@@ -45,23 +37,6 @@ func _process(_delta):
 	if not Input.is_action_pressed("accept") and move_edge:
 		move_edge = false
 	
-	#如果有目標位置且相距大於20就向那裡移動
-	if character_p != 0 and (int(game_window.position.x) - int(character_p) > 5 
-	or int(game_window.position.x) - int(character_p) < -5):
-		if game_window.position.x > character_p:
-			direction = -1
-		else:
-			direction = 1
-		game_window.velocity.x = direction * 100
-		SignalManager.emit_signal("character_walk",direction)
-		game_window.move_and_slide()
-	#如果相距小於20就判斷完成移動
-	elif character_p != 0:
-		character_p = 0
-		SignalManager.emit_signal("character_stop")
-		if study:
-			SignalManager.emit_signal("start_studing")
-	
 	
 func _input(event):
 	#正在拖移
@@ -70,14 +45,12 @@ func _input(event):
 		collision_shape_2d.position = Vector2(-31.88,-217.878)
 		SignalManager.emit_signal("character_drag")
 		moving = true
-		timer.stop()
 	elif Input.is_action_pressed("accept") and not move_edge and event is InputEventMouseMotion:
 		#沒在拖移，但正進行可能的活動（按下左鍵並拖移）
 		move_p = Vector2i(game_window.position) - DisplayServer.mouse_get_position()
 	#正在摸頭，缺點是拖移時會有一段摸頭的時期
 	if Input.is_action_pressed("accept") and in_move_area and event is InputEventMouseMotion and not move_edge:
 		SignalManager.emit_signal("character_caress","y")
-		timer.stop()
 	elif not in_move_area:
 		SignalManager.emit_signal("character_caress","n")
 		
@@ -106,21 +79,5 @@ func _on_colliding(d):
 	move_edge = false
 	SignalManager.emit_signal("character_stop")
 
-func _on_timer_timeout() -> void:
-	if not stop:
-		character_p = randi_range(-399, 237)
-		timer.wait_time = randi_range(5,15)
-
-	
-func _on_study():
-	character_p = 0
-	study = false
-	
-func _on_studing():
-	stop = true
-	
-func _end_study():
-	stop = false
-	
-func _on_room_mode():
-	pass
+func _to_study() -> void:
+	SignalManager.emit_signal("start_studing")
